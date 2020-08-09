aee5ab90f4ef1656a1e24e5c6bd4771bb27c5368
jituanlin <2607824216@qq.com>
2020-08-09 09:59:31 +0800

    fpinscala exercise432.5

--- scala-stack/fpinscala-practice/src/main/scala/Exercise.scala
+++ scala-stack/fpinscala-practice/src/main/scala/Exercise.scala
@@ -4,20 +4,10 @@ import java.util.regex.{Pattern, PatternSyntaxException}
 
 
 object answer {
-  def map2[A, B, C](oa: Option[A], ob: Option[B])(ab2c: (A, B) => C) = for {
-    a <- oa
-    b <- ob
-  } yield ab2c(a, b)
-
-  def pattern(ps: String): Option[Pattern] = {
-    try {
-      Some(Pattern.compile(ps))
-    } catch {
-      case e: PatternSyntaxException => None
-    }
-  }
-
-  def bothMatch(pa: Pattern, pb: Pattern, s: String): Boolean = pa.matcher(s).matches() && pb.matcher(s).matches()
-
-  def bothMatch2(psa: String, psb: String, s: String): Option[Boolean] = map2(pattern(psa), pattern(psb))((pa, pb) => bothMatch(pa, pb, s))
+  def sequence[A](xs: List[Option[A]]): Option[List[A]] = xs.foldLeft(Option(List[A]()))(
+    (optXs, optX) => for {
+      xs <- optXs
+      x <- optX
+    } yield xs.appended(x)
+  )
 }
--- scala-stack/fpinscala-practice/src/test/scala/ExerciseSpec.scala
+++ scala-stack/fpinscala-practice/src/test/scala/ExerciseSpec.scala
@@ -6,9 +6,12 @@ import exercise.answer
 
 class ExerciseSpec extends AnyFlatSpec {
   "basic functionality" should "work" in {
-    val isMatchedOpt = answer.bothMatch2("[ab]", "[bc]", "b")
+    val someXs = answer.sequence(List[Option[Number]](Some(1), Some(2), Some(3)))
     for {
-      isMatched <- isMatchedOpt
-    } assert(isMatched)
+      xs <- someXs
+    } assert(xs.size === 3)
+
+    val noneXs = answer.sequence(List[Option[Number]](Some(1), None))
+    assert(noneXs === None)
   }
 }
