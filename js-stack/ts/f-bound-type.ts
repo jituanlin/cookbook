c4d77ea824bd3d4f6ba7587b50cfb1800326c847
jituanlin <2607824216@qq.com>
2020-08-13 20:38:38 +0800

    add news

---
+++ js-stack/ts/f-bound-type.ts
@@ -0,0 +1,17 @@
+export class A {
+  f(): this {
+    return this;
+  }
+}
+
+export class B extends A {
+  private n = 42;
+  f1() {
+    /**
+     * The this.f return value of `this` type.
+     * It inferred as class which containing it.
+     * In this case, it is class B, which own the `n` property.
+     * */
+    console.log(this.f().n);
+  }
+}
