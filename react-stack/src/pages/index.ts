7077baa4a83657e372e2405df3b087e2cd98267d
jituanlin <2607824216@qq.com>
2020-09-05 22:44:16 +0800

    misc

--- react-stack/src/pages/index.ts
+++ react-stack/src/pages/index.ts
@@ -1,2 +1,3 @@
 export { StaleClosureExample } from "./stale-closure-example";
 export { UseEffectToSetStateExample } from "./use-use-effect-to-set-state";
+export { WhetherReactUpdateIfPropsNoChanged } from "./whether-react-update-if-props-nochanged";
---
+++ react-stack/src/pages/whether-react-update-if-props-nochanged/index.tsx
@@ -0,0 +1,57 @@
+/**
+ * Parent component rendering will trigger children component.
+ * Because by default, children component 's `shouldComponentUpdate` return true.
+ * */
+import React from "react";
+
+export class WhetherReactUpdateIfPropsNoChanged extends React.Component<
+  {},
+  { n: number }
+> {
+  state = { n: 0 };
+
+  componentDidMount() {
+    setTimeout(() => {
+      console.log("parent state will be changed");
+      console.log("-----------------------------------");
+      this.setState(({ n }) => ({ n: n + 1 }));
+    }, 1000);
+  }
+
+  componentDidUpdate(
+      prevProps: Readonly<{
+        handler: () => void;
+      }>,
+      prevState: Readonly<{}>,
+      snapshot?: any
+  ) {
+    console.log("parent component has updated");
+  }
+
+  render() {
+    console.log("parent component render");
+    return (
+      <div>
+        <p>{this.state.n}</p>
+        <Child />
+      </div>
+    );
+  }
+}
+
+class Child extends React.Component<{}, {}> {
+  render() {
+    console.log("child component render");
+    return <p>Child Component</p>;
+  }
+
+  componentDidUpdate(
+    prevProps: Readonly<{
+      handler: () => void;
+    }>,
+    prevState: Readonly<{}>,
+    snapshot?: any
+  ) {
+    console.log("child component has updated");
+  }
+}
