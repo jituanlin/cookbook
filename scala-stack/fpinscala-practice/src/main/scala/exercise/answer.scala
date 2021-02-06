9ac67e92bef50172eff630392d98dba13f7a2bc0
jituanlin <jituanlin@tencent.com>
2021-02-06 20:47:25 +0800

    exercise5.2

--- scala-stack/fpinscala-practice/src/main/scala/exercise/answer.scala
+++ scala-stack/fpinscala-practice/src/main/scala/exercise/answer.scala
@@ -1,33 +1,38 @@
 package exercise
 
+import exercise.answer.Stream.cons
+
 object answer {
 
+  case object empty extends Stream[Nothing]
+
+  case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
   trait Stream[+A] {
-    def uncons: Option[(A, Stream[A])]
-
-    def isEmpty: Boolean = uncons.isEmpty
-
-    def toList: List[A] = uncons match {
-      case Some((hd, tl)) => hd :: tl.toList
-      case None => List.empty
+    def take(n: Int): Stream[A] = this match {
+      case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+      case Cons(h, _) => Stream(h())
+      case _ => empty
     }
 
+    def toList: List[A] = this match {
+      case Cons(h, t) => h() :: t().toList
+      case _ => List()
+    }
   }
 
   object Stream {
-    def empty[A]: Stream[A] =
-      new Stream[A] {
-        def uncons = None
-      }
-
-    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] =
-      new Stream[A] {
-        lazy val uncons = Some((hd, tl))
-      }
+    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+      lazy val head = hd
+      lazy val tail = tl
+      Cons(() => head, () => tail)
+    }
 
     def apply[A](as: A*): Stream[A] =
       if (as.isEmpty) empty
       else cons(as.head, apply(as.tail: _*))
+
+
   }
 
 }
--- scala-stack/fpinscala-practice/src/test/scala/exercise/spec/ExerciseSpec.scala
+++ scala-stack/fpinscala-practice/src/test/scala/exercise/spec/ExerciseSpec.scala
@@ -1,12 +1,14 @@
 package exercise.spec
 
 import org.scalatest.flatspec.AnyFlatSpec
-import  exercise.answer.Stream
+import exercise.answer.{empty, Stream}
 
 
 class ExerciseSpec extends AnyFlatSpec {
   "basic functionality" should "work" in {
     val s = Stream(1, 2, 3)
-    assert(s.toList.equals(List(1, 2, 3)))
+    assert(s.take(1).toList.equals(List(1)))
+    assert(s.take(4).toList.equals(List(1, 2, 3)))
+    assert(empty.take(4).toList.equals(List.empty))
   }
 }
